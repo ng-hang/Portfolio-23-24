699ddb95674e9b68b2d30eadcb79c8aed7a56bc1
ng-hang <160196731+ng-hang@users.noreply.github.com>
2024-02-16 03:23:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,191 @@
+let layer1 = document.getElementById('layer1');
+let layer2 = document.getElementById('layer2');
+let layer3 = document.getElementById('layer3');
+let layer4 = document.getElementById('layer4');
+let layer5 = document.getElementById('layer5');
+let layer6 = document.getElementById('layer6');
+let layer7 = document.getElementById('layer7');
+let layer8 = document.getElementById('layer8');
+let layer9 = document.getElementById('layer9');
+let layer10 = document.getElementById('layer10');
+
+window.addEventListener('scroll', ()=> {
+	let value = window.scrollY;
+	layer10.style.top = value*-1.5 +'pt';
+	layer9.style.top = value*-0.9 +'pt';
+	layer8.style.top = value*-0.8 +'pt';
+	layer7.style.top = value*-0.7 +'pt';
+	layer7.style.left = value*0.7 +'pt';
+	layer6.style.top = value*-0.6 +'pt';
+	layer5.style.top = value*-0.5 +'pt';
+	layer5.style.left = value*-0.5 +'pt';
+	layer4.style.top = value*-0.4 +'pt';
+	layer3.style.top = value*-0.3 +'pt';
+	layer3.style.rotate =value*-0.1+'deg';
+	layer2.style.top = value*-0.2 +'pt';
+	layer1.style.top = value*0 +'pt';
+});
+
+function myFunction0() {
+  var x = document.getElementById("myDIV0");
+  var y = document.getElementById("change0");
+  var z = document.getElementById("title0");
+  var t = document.getElementById("icon");0
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunction1() {
+  var x = document.getElementById("myDIV1");
+  var y = document.getElementById("change1");
+  var z = document.getElementById("title1");
+  var t = document.getElementById("icon1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunction2() {
+  var x = document.getElementById("myDIV2");
+  var y = document.getElementById("change2");
+  var z = document.getElementById("title2");
+  var t = document.getElementById("icon2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunction3() {
+  var x = document.getElementById("myDIV3");
+  var y = document.getElementById("change3");
+  var z = document.getElementById("title3");
+  var t = document.getElementById("icon3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunction4() {
+  var x = document.getElementById("myDIV4");
+  var y = document.getElementById("change4");
+  var z = document.getElementById("title4");
+  var t = document.getElementById("icon4");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+
+function myFunction5() {
+  var x = document.getElementById("myDIV5");
+  var y = document.getElementById("change5");
+  var z = document.getElementById("title5");
+  var t = document.getElementById("icon5");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+
+function myFunctiona() {
+  var x = document.getElementById("myDIVa");
+  var y = document.getElementById("changea");
+  var z = document.getElementById("titlea");
+  var t = document.getElementById("icona");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunctionb() {
+  var x = document.getElementById("myDIVb");
+  var y = document.getElementById("changeb");
+  var z = document.getElementById("titleb");
+  var t = document.getElementById("iconb");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
+
+function myFunctionc() {
+  var x = document.getElementById("myDIVc");
+  var y = document.getElementById("changec");
+  var z = document.getElementById("titlec");
+  var t = document.getElementById("iconc");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.backgroundColor= "";
+        z.style.color= "";
+    t.style.color= "";
+  } else {
+    x.style.display = "block";
+    y.style.backgroundColor= "#ca6702";
+    z.style.color= "#e9d8a6";
+    t.style.color= "#e9d8a6";
+  }
+};
